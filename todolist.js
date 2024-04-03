5494c3d71a168fcc930c4f2db8eafee55c3e9fed
DewenLei04 <leidewen2004@gmail.com>
2024-04-03 16:21:51 -0400

    Initial commit

--- todolist.js
+++ todolist.js
@@ -1,1 +1,29 @@
-hey
+let input = prompt('what would you like to do?');
+const todos = [''];
+
+
+while (input !== 'quit' && input !== 'q') {
+    input = prompt("What you want to do?");
+    if (input === 'list') {
+        console.log('************');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('************');
+    } else if (input === 'new') {
+        const newTodo = prompt("Enter new task:");
+        todos.push(newTodo);
+        console.log(`${newTodo} is added to the list`);
+    } else if (input === 'delete') {
+        const index = parseInt(promt('enter a index to delete'));
+        if (!Number.isNan(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log('deleted');
+        } else {
+            console.log('Invalid index');
+        }
+    }
+    console.log('what you want todo');
+}
+
+console.log("Goodbye!");
